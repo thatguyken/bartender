c538dc1bd42d14152f4029f9d414ffbc2c60d5f2
thatguyken <thatguyken@berkeley.edu>
2017-12-08 02:31:09 -0800

    Final modifications to main code

--- src/track/src/IKmove.py
+++ src/track/src/IKmove.py
@@ -14,22 +14,11 @@ class IKmove(object):
         self.compute_ik = rospy.ServiceProxy('compute_ik', GetPositionIK)
     
     def move(self, targetPos, targetOr):
-        #Wait for the IK service to become available
-        #rospy.wait_for_service('compute_ik')
-        #rospy.init_node('service_query')
-        
-        #Create the function used to call the service
-        #compute_ik = rospy.ServiceProxy('compute_ik', GetPositionIK)
-        
-        #while not rospy.is_shutdown():
-        #raw_input('Press [ Enter ]: ')
-        
-        #2 = .03
-        #Construct the request
+
         request = GetPositionIKRequest()
         request.ik_request.group_name = "left_arm"
         request.ik_request.ik_link_name = "left_gripper"
-        request.ik_request.attempts = 30
+        request.ik_request.attempts = 20
         request.ik_request.pose_stamped.header.frame_id = "base"
         
         #Set the desired orientation for the end effector HERE
@@ -41,49 +30,26 @@ class IKmove(object):
         request.ik_request.pose_stamped.pose.orientation.z = targetOr[2]
         request.ik_request.pose_stamped.pose.orientation.w = targetOr[3]
 
-
-        
         try:
             #Send the request to the service
             response = self.compute_ik(request)
             
-            #Print the response HERE
-            #print(response)
             group = MoveGroupCommander("left_arm")
-            #group.set_goal_tolerance(toler)
-
+            group.clear_pose_targets()
+            group.clear_path_constraints()
             group.set_pose_target(request.ik_request.pose_stamped)
             group.set_start_state_to_current_state()
-
-            # orien_const = OrientationConstraint()
-            # orien_const.link_name = "left_gripper"
-            # orien_const.header.frame_id = "base"
-            # # orien_const.orientation.x = 0
-            # # orien_const.orientation.y = 0.5
-            # # orien_const.orientation.z = 0
-            # orien_const.orientation.w = -1.0
-
-            # orien_const.absolute_x_axis_tolerance = 0.1
-            # orien_const.absolute_y_axis_tolerance = 0.1
-            # orien_const.absolute_z_axis_tolerance = 0.1
-            # orien_const.weight = 1.0
-            # consts = Constraints()
-            # consts.orientation_constraints = [orien_const]
-            # group.set_path_constraints(consts)
 
             # Setting position and orientation target
             group.set_pose_target(request.ik_request.pose_stamped)
 
-            # TRY THIS
-            # Setting just the position without speciftying the orientation
-            ###group.set_position_target([0.5, 0.5, 0.0])
-
-
             # Plan IK and execute
             pl = group.plan()
             group.execute(pl)
-            # group.stop()
-            # group.go()
+            group.forget_joint_values("left_w2")   
+            group.stop()
+            group.clear_pose_targets()
+            group.clear_path_constraints()
             
         except rospy.ServiceException, e:
             print "Service call failed: %s"%e
--- src/track/src/main.py
+++ src/track/src/main.py
@@ -15,29 +15,17 @@ from std_msgs.msg import String, Int16MultiArray
 from geometry_msgs.msg import Pose,Vector3,Quaternion,PoseStamped
 from sensor_msgs.msg import Image
 from copy import deepcopy
-
 import argparse
 import baxter_external_devices
 import numpy as np
 
-''' CHANGE all orientations self.GetEndPointOr() '''
 
 
 current_bottle_set = []
-
-#original
-#home_trans = [0.747, 0.261, 0.140]
-#home_qua = [-0.044, 0.775, -0.044, 0.629]
-
-# home_trans = [0.810, -0.669, 0.436]
-# home_qua = [0.821, -0.224, 0.519, 0.085]
-
 home_trans = [0.818, 0.305, 0.192]
 home_qua = [0.014, 0.713, -0.050, 0.699]
-
-
 Bottle_Position = [0.0,0.0,0.0]
-Cup_Position = [0.0,0.0,0.0] #9
+Cup_Position = [0.0,0.0,0.0] 
 
 def callback1(msg):
         # Callback for found_markers
@@ -49,20 +37,18 @@ class makeDrink(object):
         self.current_bottle_set = [10,11,12,13]
         self.Bottle_Position = [0.0,0.0,0.0]
         self.Cup_Position = [0.0,0.0,0.0]
-
         self.drink_bottle_map = {'2':[10,11], 'manhattan':[10, 11, 12], 'martini':[11, 12, 13], 'white russian':[10, 12, 13], 'rum and coke':[12, 13], 't':[13]}
-        #self.cup = 14
-        ##### Initialize AR_tag transforms #####
+
+        #Initialize AR_tag transforms 
         rospy.init_node('master', anonymous=True)
         self.listener = tf.TransformListener() 
         
-
-        ##### Initialize Inverse Kinematics #####  
+        # Initialize Inverse Kinematics 
         self.Counting = 0
-        #Initialize moveit_commander
+        # Initialize moveit_commander
         moveit_commander.roscpp_initialize('/joint_states:=/robot/joint_states')
-        #rospy.init_node('moveit_node')
-        #Initialize MoveIt for arm
+
+        # Initialize MoveIt for arm
         self.robot = moveit_commander.RobotCommander()
         self.scene = moveit_commander.PlanningSceneInterface()
         self.Arm_left = baxter_interface.Limb('left')
@@ -71,30 +57,31 @@ class makeDrink(object):
         self.left_arm.set_goal_orientation_tolerance(0.01)
         self.left_arm.set_planner_id('RRTConnectkConfigDefault')
         self.left_arm.set_planning_time(10) 
-        ##### Initialize Gripper control #####
+
+        # Initialize Gripper control
         self.left_gripper = robot_gripper.Gripper('left')
         #Calibrate the gripper (other commands won't work unless you do this first)
         print('Calibrating...')
         self.left_gripper.calibrate()
 
-        ##### Verify robot is enabled  #####
+        # Verify robot is enabled  
         print("Getting robot state... ")
         self._rs = baxter_interface.RobotEnable(CHECK_VERSION)
         self._init_state = self._rs.state().enabled
         print("Enabling robot... ")
         self._rs.enable()
         print("Running. Ctrl-c to quit")
-        #self.current_bottle_set = []
+
 
     def GetEndPointPosition(self):
-        rospy.sleep(1.0) # i was tired when I added this
+        rospy.sleep(0.5)
         for i in range(50):
             POSE = self.Arm_left.endpoint_pose()
             EndPoint = [POSE['position'].x ,POSE['position'].y, POSE['position'].z]
         return EndPoint
 
     def GetEndPointOr(self):
-        rospy.sleep(1.0) # i was tired when I added this
+        rospy.sleep(0.5) 
         for i in range(50):
             POSE = self.Arm_left.endpoint_pose()
             EndPoint = [POSE['orientation'].x ,POSE['orientation'].y, POSE['orientation'].z, POSE['orientation'].w]
@@ -122,6 +109,7 @@ class makeDrink(object):
         print "==============="
         print "FIND BOTTLE"
         print "==============="
+
         #Get information about the markers found
         rospy.Subscriber("/track/found_markers",Int16MultiArray, callback1)
         msg1 = rospy.wait_for_message('/track/found_markers',Pose)
@@ -136,8 +124,7 @@ class makeDrink(object):
                 position = Vector3()
                 orientation = Quaternion()
                 targetMarker = 'ar_marker_'+str(chosenMarker)
-                print "the marker frame chosen", targetMarker
-                #print(targetMarker.position)              
+                print "the marker frame chosen", targetMarker      
                 (trans,rot) = self.listener.lookupTransform('/base',targetMarker,rospy.Time(0))
 
                 Px = trans[0]
@@ -205,32 +192,11 @@ class makeDrink(object):
         else:
             print "the cup is obscured"
 
-    '''@KEN call AR tag function here and set values as bottle_pos, find a good home pos, '''
-    # home_pos = 
-    # accuracy = 0.005 # 0.005 is accurate, 0.3 is rough
-    # pour.move_to_bottle(self.GetEndPointPosition(), home_pos, bottle_pos, accuracy)
-    # grab_bottle(arm, left_gripper, self.GetEndPointPosition())
-
     def bottleToCup(self):
         print "==============="
         print "BOTTLE TO CUP"
         print "==============="
         ik = IKmove()
-        #self.FindBottle()
-        #move to cup
-        # self.Arm_left.set_joint_position_speed(1.0)
-        # cupPos = deepcopy(self.Cup_Position)
-        # cupPos[0] -= 0.08 
-        # cupPos[2] += 0.20 #above cup
-        # ik.move(cupPos,self.GetEndPointOr())
-        # currpos = self.GetEndPointPosition()
-
-
-        # #move up
-        # currpos[2] += 0.15
-        # ik.move(currpos,self.GetEndPointOr())
-        # currpos = self.GetEndPointPosition()
-
 
         print "move to the left of the cup"
         cupPos = deepcopy(self.Cup_Position)
@@ -242,67 +208,45 @@ class makeDrink(object):
         print "move forward, beside cup"
         currpos[0] += 0.09
         ik.move(currpos,self.GetEndPointOr())
-        #currpos = self.GetEndPointPosition()
-
-
-
-
+ 
     def pickUpBottle(self):
         print "==============="
         print "PICK UP BOTTLE"
         print "==============="
         ik = IKmove()
-        #self.returnHome()
-        #ik.move(home_trans,home_qua)
-        #print("!!!!CUP!!!!", self.Cup_Position)
-        #self.Arm_left.set_joint_position_speed(1.0)
         self.left_gripper.open()
         botpos = deepcopy(self.Bottle_Position)
-        #self.FindBottle()
         botpos[0] -= .06
         
         print "move to bottle"
         ik.move(botpos,self.GetEndPointOr())
         currpos = self.GetEndPointPosition()
-        rospy.sleep(2.0)
+        rospy.sleep(0.5)
 
         print "move forward"
         currpos[0] += .105
         ik.move(currpos,self.GetEndPointOr())
         self.left_gripper.close()
-        rospy.sleep(2.0)
+        rospy.sleep(0.5)
 
         print "move up"
         currpos = self.GetEndPointPosition()
-        # currpos[0] -= .08
         currpos[2] += .10
         ik.move(currpos,self.GetEndPointOr())
 
         print "move back"
         currpos = self.GetEndPointPosition()
-        #currpos[0] -= .08
-        currpos[0] -= .06 #CHECK, might need to increase so that you dont knock over other things (was 0.5)
+        currpos[0] -= .06 
         ik.move(currpos,self.GetEndPointOr())
 
-        #self.left_gripper.open()
         self.returnHome()
 
 
-        #ik.move(home_trans,home_qua)
-        # self.FindCup()
-        # cuppos = self.Cup_Position
-        # cuppos[1] += .15
-        # ik.move(cuppos,home_qua)
-
     def pourSequence(self):
-        self.returnHome() #delete later
-        rospy.sleep(3.0)
+        self.returnHome()
+        rospy.sleep(0.5)
         self.FindBottle()
-        rospy.sleep(2.0)
-        # self.FindCup()
-        rospy.sleep(3.0)
-
-        #self.returnHome()
+        rospy.sleep(0.5)
         self.pickUpBottle()
         self.bottleToCup()
         self.move_wrist()
@@ -312,28 +256,9 @@ class makeDrink(object):
 
 
     def primaryFunction(self):
-        #self.returnHome()
-        #self.move_wrist()
-        #self.returnHomeFK()
-
-        # self.straighten_elbow()
-
-        #self.returnHomeFK() #UPDATE JOANA
-        # while not rospy.is_shutdown():
-        #     #post = [0.65, 0.123, -0.073]
-        #     # USEFUL
-        #     # self.pickUpBottle()
-        #     # self.bottleToCup()
-        #     # self.move_wrist()
-        #     # self.returnHome()
-        #     # self.returnBottle()
-        #     # self.returnHome()
-
-        #     ''' Main sequence of commands '''
         self.FindCup()
         drink = raw_input('Please enter the name of the drink you would like to make\t')
         self.mapDrinktoBottles(drink)
-
         self.pourSequence()
 
         #remove that bottle from current bottle set
@@ -351,20 +276,14 @@ class makeDrink(object):
         desired_pos = current_position 
         desired_pos[3] -= .52
 
-    # def scanPos(self):
-
-
-
-            
 
     def returnBottle(self):
         print "==============="
         print "RETURN BOTTLE"
         print "==============="
         ik = IKmove()
-        #self.Arm_left.set_joint_position_speed(1.0)
-        aboveBottle = deepcopy(self.Bottle_Position)
 
+        aboveBottle = deepcopy(self.Bottle_Position)
 
         #move above bottle
         aboveBottle[2] += .1 
@@ -384,7 +303,7 @@ class makeDrink(object):
 
         #let go of bottle
         self.left_gripper.open()
-        rospy.sleep(1.0)
+        rospy.sleep(.5)
 
         #back away from bottle
         currpos = self.GetEndPointPosition()
@@ -400,6 +319,7 @@ class makeDrink(object):
         for t, n in zip(angles, names):
             commands[n] = t
         print(commands)
+
         limb = self.Arm_left
         current_position = [limb.joint_angle(n) for n in names]
         diffs = [abs(c - w) for c, w in zip(current_position, angles)]
@@ -407,58 +327,32 @@ class makeDrink(object):
             limb.set_joint_positions(commands)
             current_position = [limb.joint_angle(n) for n in names] 
             diffs = [abs(c - w) for c, w in zip(current_position, angles)]
-        
+
+
+
     def move_wrist(self):
         print "==============="
         print "MOVE WRIST"
         print "==============="
         limb = self.Arm_left
-        self.Arm_left.set_joint_position_speed(0.50) #NEW
+        self.Arm_left.set_joint_position_speed(.25) 
         pour_time = 1.0
         deg90 = 1.57
         deg30 = .52
         names = ['left_s0', 'left_s1', 'left_e0', 'left_e1', 'left_w0', 'left_w1', 'left_w2']
 
         # rotate to 90 deg
+        ik = IKmove()
         rospy.sleep(2.0)
         current_position = [limb.joint_angle(n) for n in names]
         desired_pos = current_position 
         desired_pos[6] += deg90 + deg30
         self.fk(desired_pos)
-        rospy.sleep(2.0)
         self.Arm_left.set_joint_position_speed(1.0)
-
-        #WHY DOES IT TURN BACK ON ITS OWN???????????????????????
-
-
-        # #rotate 30 deg for set amount of time
-        # current_position = [limb.joint_angle(n) for n in names]
-        # desired_pos = current_position 
-        # desired_pos[6] -= deg30
-        # self.fk(desired_pos)
-        # rospy.sleep(pour_time)
-
-        # # straighten bottle
-        # current_position = [limb.joint_angle(n) for n in names]
-        # desired_pos = current_position 
-        # desired_pos[6] += (deg90 + deg30)
-        # self.fk(desired_pos)
-        # rospy.sleep(1.0)
 
     def returnHomeFK(self):
         limb = self.Arm_left
         names = ['left_s0', 'left_s1', 'left_e0', 'left_e1', 'left_w0', 'left_w1', 'left_w2']
-
-        # curr name: ['head_nod', 'head_pan', 'left_e0', 'left_e1', 'left_s0', 'left_s1', 'left_w0', 'left_w1', 'left_w2', 'right_e0', 'right_e1', 'right_s0', 'right_s1', 'right_w0', 'right_w1', 'right_w2', 'torso_t0']
-        # curr position: [0.0, -0.025310683000109326, -0.011504855909140603, 2.4482333374651204, -0.7251894174728294, -0.48742239535059023, 0.13115535736420286, -1.5715633171886063, -0.06519418348513008, -3.045718854346489, 1.6896798378557834, 0.3160000423043952, 0.6009369736541108, 0.048703890015361885, -1.5558400141127808, -0.006135923151541655, -12.565987119160338]
-
-
-        # name: ['head_nod', 'head_pan', 'left_e0', 'left_e1', 'left_s0', 'left_s1', 'left_w0', 'left_w1', 'left_w2', 'right_e0', 'right_e1', 'right_s0', 'right_s1', 'right_w0', 'right_w1', 'right_w2', 'torso_t0']
-        # position: [0.0, -0.0279951493789088, 0.06289321230330196, 2.50767509299568, -0.25579129637989273, -0.7217379607000872, 0.6538593108361576, -1.513655542445932, 0.030679615757708275, -0.01687378866673955, 2.0252381352057176, 0.23623304133435372, -0.863631183579488, -3.0441848735586037, -0.2841699409557729, -2.9272188384823408, -12.565987119160338]
-
-
-
-
         desired_pos = [-0.7251894174728294, -0.48742239535059023, -0.011504855909140603, 2.4482333374651204, 0.13115535736420286, -1.5715633171886063, -0.06519418348513008]
         self.fk(desired_pos)
         rospy.sleep(3.0)
